604681ef30b764d284e88db670c9c094d75fc33c
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-26 15:31:31 +0000

    Minor Errors

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let numone = 0
 let numtwo = 0
 let numthree = 0
 let answer = 0
-let calculation = 0
 let counter = 0
 let negative = false
 
@@ -19,13 +18,11 @@ function button () {
     negative = !negative * -1
     numone = Math.abs(numone)
    }
-
    if (numtwo < 0) {
     negative = !negative * -1
     numtwo = Math.abs(numtwo)
    }
 
-  
   for (counter = 0; counter < numone; counter = counter + 1) {
     answer = answer + numtwo
   }
