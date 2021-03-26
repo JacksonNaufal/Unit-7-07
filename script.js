86d65d406a988d615761a93be7aecced03449462
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-26 16:51:56 +0000

    Added comments

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-document.getElementById('button').addEventListener('click', button)
-
 let numone = 0
 let numtwo = 0
 let numthree = 0
 let answer = 0
 let counter = 0
 let negative = false
+// These are the variables for each of my words, or numbers etc.
+
+document.getElementById('button').addEventListener('click', button)
+// This is my event listenr so the program knows what to use, and what to click
+
 
 function button () {
   numone = document.getElementById('input').value
@@ -13,6 +16,8 @@ function button () {
   numone = parseInt(numone)
   numtwo = parseInt(numtwo)
   numthree = parseInt(numthree)
+
+  // These are my functions, so the input box knows what to use when numbers are inputted into it
 
   if (numone < 0) {
     negative = !negative * -1
@@ -32,3 +37,5 @@ function button () {
   document.getElementById('answer').innerHTML =
   answer
 }
+
+// Those are my ifs statements. which allows the counter to increace everytime we hit calculate, and it also allows the calculator to know if when there a - if the number is true or false, etc.
