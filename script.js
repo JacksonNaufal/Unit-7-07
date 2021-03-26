b540bf91841c773463620345f9a4095e2cd83520
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-26 14:08:06 +0000

    JS Errors

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-document.getElementById('button').addEventListener('click',button)
+document.getElementById('button').addEventListener('click', button)
 
 let numone = 0
 let numtwo = 0
 let numthree = 0
 let answer = 0
 let calculation = 0
+let counter = 0
 
 function button () {
-  numone = document.getElementById('input').value 
-  numtwo = document.getElementById('input2').value 
+  numone = document.getElementById('input').value
+  numtwo = document.getElementById('input2').value
   numone = parseInt(numone)
   numtwo = parseInt(numtwo)
   numthree = parseInt(numthree)
-  
-  if (numone || numtwo < 0) {
-    document.getElementById('answer').value = '-'
-  }
 
-for (counter = 0; counter < numone; counter = counter + 1) {
+
+  for (counter = 0; counter < numone; counter = counter + 1) {
     answer = answer + numtwo
     calculation = calculation - numtwo
  }
  document.getElementById('answer').innerHTML =
- answer
- document.getElementById('answertwo').innerHTML =
+  answer
+  document.getElementById('answertwo').innerHTML =
  calculation
-}
+ }
 
 
 
